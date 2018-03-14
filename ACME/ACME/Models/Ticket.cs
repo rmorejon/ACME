809745f1d0d76dc4dc264640a7935175bfd0ec4f
Rolando Morejón Mayedo <rolando.morejon@datys.cu>
2018-03-14 13:20:08 -0400

    (+)Add model for Ticket

---
+++ ACME/ACME/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿namespace ACME.Models
+{
+    using System;
+
+    public class Ticket
+    {
+        #region Properties Public
+
+        public int TicketId { get; set; }
+
+        public User User { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        #endregion
+    }
+}
