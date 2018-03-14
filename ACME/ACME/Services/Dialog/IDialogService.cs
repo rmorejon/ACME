30d8427a29efd0d8e9d2c32617472b9092faa9f5
Rolando Morejón Mayedo <rolando.morejon@datys.cu>
2018-03-14 13:19:11 -0400

    (+)Add dialog service

---
+++ ACME/ACME/Services/Dialog/IDialogService.cs
@@ -0,0 +1,22 @@
+﻿namespace ACME.Services.Dialog
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IDialogService
+    {
+        #region Public Methods
+
+        Task ShowAlertAsync(string message, string title, string buttonLabel);
+
+        void ShowToast(string message, int duration = 5000);
+
+        Task<bool> ShowConfirmAsync(string message, string title, string okLabel, string cancelLabel);
+
+        Task<string> SelectActionAsync(string message, string title, IEnumerable<string> options);
+
+        Task<string> SelectActionAsync(string message, string title, string cancelLabel, IEnumerable<string> options);
+
+        #endregion
+    }
+}
