db7be16959ce68bfee6f3b049ddd9ddd9fe25820
Rolando Morejón Mayedo <rolando.morejon@datys.cu>
2018-03-14 13:18:10 -0400

    (+)Add the viewmodel for DashBoardPage

---
+++ ACME/ACME/ViewModels/DashBoardPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿namespace ACME.ViewModels
+{
+    using System;
+    using System.Collections.ObjectModel;
+
+    using ACME.Models;
+
+    using GalaSoft.MvvmLight;
+
+    public class DashBoardPageViewModel : ViewModelBase
+    {
+        public ObservableCollection<Ticket> Tickets { get; set; }
+
+        public DashBoardPageViewModel()
+        {
+            this.Tickets = new ObservableCollection<Ticket>();
+            this.Tickets.Add(new Ticket()
+                                 {
+                                     User = new User()
+                                                {
+                                                    Id = "1",
+                                                    LastName = "moreon",
+                                                    Name = "Rolando",
+                                                    Password = "12",
+                                                    Phone = "124423"
+                                                },
+                                     DateTime = DateTime.Now,
+                                     Address = "Lisa",
+                                     TicketId = 1280
+                                 });
+
+            this.Tickets.Add(new Ticket()
+                                 {
+                                     User = new User()
+                                                {
+                                                    Id = "2",
+                                                    LastName = "moreon",
+                                                    Name = "Pepe",
+                                                    Password = "12",
+                                                    Phone = "2131444"
+                                                },
+                                     DateTime = DateTime.Now,
+                                     Address = "Marianao",
+                                     TicketId = 1286
+                                 });
+        }
+    }
+}
