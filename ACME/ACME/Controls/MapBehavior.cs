f05d2aaa169f3386ecce4eb78e43caede21d508f
Rolando Morejón Mayedo <rolando.morejon@datys.cu>
2018-03-19 12:42:27 -0400

    (+)Created to be able to bind the list pins

---
+++ ACME/ACME/Controls/MapBehavior.cs
@@ -0,0 +1,48 @@
+﻿namespace ACME.Controls
+{
+    using System.Collections.Generic;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Maps;
+
+    public class MapBehavior : Map
+    {
+        #region Static Fields and Constants
+
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.CreateAttached(
+            "ItemsSource",
+            typeof(IEnumerable<Pin>),
+            typeof(MapBehavior),
+            default(IEnumerable<Pin>),
+            BindingMode.Default,
+            null,
+            OnItemsSourceChanged);
+
+        #endregion
+
+        #region Properties Public
+
+        public IEnumerable<Pin> ItemsSource
+        {
+            get => (IEnumerable<Pin>)this.GetValue(ItemsSourceProperty);
+            set => this.SetValue(ItemsSourceProperty, value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static void OnItemsSourceChanged(BindableObject view, object oldValue, object newValue)
+        {
+            if (view is MapBehavior mapBehavior)
+            {
+                mapBehavior.Pins.Clear();
+                var items = (IList<Pin>)newValue;
+
+                foreach (var pin in items) mapBehavior.Pins.Add(pin);
+            }
+        }
+
+        #endregion
+    }
+}
