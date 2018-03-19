2e0ae1e2a6362d0c8d4c0d0be95ab2e3003dbc0d
Rolando Morejón Mayedo <rolando.morejon@datys.cu>
2018-03-19 12:37:26 -0400

    (+)Render to modify seletected in list view

---
+++ ACME/ACME.iOS/Renderers/ExtendedListViewCellRenderer.cs
@@ -0,0 +1,27 @@
+﻿using ACME.iOS.Renderers;
+
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(ExtendedListViewCellRenderer))]
+
+namespace ACME.iOS.Renderers
+{
+    using UIKit;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Platform.iOS;
+
+    public class ExtendedListViewCellRenderer : ViewCellRenderer
+    {
+        #region Public Methods
+
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            cell.SelectionStyle = UITableViewCellSelectionStyle.None;
+            return cell;
+        }
+
+        #endregion
+    }
+}
