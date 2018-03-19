ed89de9e1308444f40432ca1f088e6563554b80b
Rolando Morejón Mayedo <rolando.morejon@datys.cu>
2018-03-19 12:43:02 -0400

    (+)Converter to negation boolean

---
+++ ACME/ACME/Converters/BooleanNegationConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace ACME.Converters
+{
+    using System;
+    using System.Globalization;
+
+    using Xamarin.Forms;
+
+    public class BooleanNegationConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        #endregion
+    }
+}
