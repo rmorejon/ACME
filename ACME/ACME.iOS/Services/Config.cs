a3bf2deb29bc50b252cabc74d55c8cb91cbafa7b
Rolando Morejón Mayedo <rolando.morejon@datys.cu>
2018-03-19 12:41:03 -0400

    (+)Service to obtain the address of the database

---
+++ ACME/ACME.iOS/Services/Config.cs
@@ -0,0 +1,54 @@
+﻿using ACME.iOS.Services;
+
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Config))]
+
+namespace ACME.iOS.Services
+{
+    using System;
+    using System.IO;
+
+    using ACME.Interfaces;
+
+    using SQLite.Net.Interop;
+    using SQLite.Net.Platform.XamarinIOS;
+
+    public class Config : IConfig
+    {
+        #region Fields
+
+        private string directoryDB;
+
+        private ISQLitePlatform platform;
+
+        #endregion
+
+        #region IConfig Members
+
+        public string DirectoryDB
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.directoryDB))
+                {
+                    var directory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                    this.directoryDB = Path.Combine(directory, "..", "Library");
+                }
+
+                return this.directoryDB;
+            }
+        }
+
+        public ISQLitePlatform Platform
+        {
+            get
+            {
+                if (this.platform == null) this.platform = new SQLitePlatformIOS();
+                return this.platform;
+            }
+        }
+
+        #endregion
+    }
+}
