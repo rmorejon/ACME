dada5da1bf0cab30279793b76719f9cbfefeb776
Rolando Morejón Mayedo <rolando.morejon@datys.cu>
2018-03-19 12:45:42 -0400

    (+)Interface to get the directory of the bd

---
+++ ACME/ACME/Interfaces/IConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace ACME.Interfaces
+{
+    using SQLite.Net.Interop;
+
+    public interface IConfig
+    {
+        #region Properties Public
+
+        string DirectoryDB { get; }
+
+        ISQLitePlatform Platform { get; }
+
+        #endregion
+    }
+}
